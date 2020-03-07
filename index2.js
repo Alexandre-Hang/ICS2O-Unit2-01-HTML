917f7b0f06d0d053c502dfc90c01ce8a6dc8490d
Ubuntu <ubuntu@ip-172-31-93-59.ec2.internal>
2020-03-07 18:15:03 +0000

    Added index2.html with javascript

---
+++ index2.js
@@ -0,0 +1,7 @@
+// JavaScript File
+var str = new String("Hello, World!");
+document.write(str.fontcolor( "red" ));
+alert(str.fontcolor( "red" ));
+var hds = new String("Learning javascript");
+document.write(hds);
+alert( 'Learning javascript' );
